ccb1adfe1a65435c633c330e2a5826281364724a
jsd <rails -v>
2014-11-04 21:56:20 +0500

    User patch request is now working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user.api_key.token.to_json, status: :created, location: @user
+      render json: @user.api_key.token.to_json, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
     @user = @current_user
 
     if @user.update(user_params)
-      head :no_content
+      render json: @user, status: :accepted
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password,
-                                 :password_confirmation, :lat, :lng)
+                                 :password_confirmation, :lat, :lon)
   end
 end
